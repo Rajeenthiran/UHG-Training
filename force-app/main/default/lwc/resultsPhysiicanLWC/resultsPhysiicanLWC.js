26e833802f06e88a33ebdc8d5e45e7d5106a0423
Rajeenthiran Thangenthiran <rajee781996@gmail.com>
2021-08-10 23:01:29 +0530

    insert function added

--- force-app/main/default/lwc/resultsPhysiicanLWC/resultsPhysiicanLWC.js
+++ force-app/main/default/lwc/resultsPhysiicanLWC/resultsPhysiicanLWC.js
@@ -1,3 +1,45 @@
-import { LightningElement } from 'lwc';
+import { LightningElement,api,track } from 'lwc';
+import insertPhysicianList from '@salesforce/apex/PhysicianController.insertPhysician';
+export default class ResultsPhysiicanLWC extends LightningElement {
+    @api phy;
+    @track physicianD=[];
+    @track succesMsg='';
+    @track errorMsg='';
+    @track phyNumber=''
 
-export default class ResultsPhysiicanLWC extends LightningElement {}
+    handleInsertPhysician(){
+       
+      if(this.phy!=null){
+         let phyObject={
+             phyname:this.phy.basic.name,
+             gender:this.phy.basic.gender,
+                pid:this.phy.number,
+             mobile:this.phy.addresses[0].telephone_number,
+             address:this.phy.addresses[0].address_1,
+             city:this.phy.addresses[0].city,
+             state:this.phy.addresses[0].state,
+             zip:this.phy.addresses[0].postal_code,
+             country:this.phy.addresses[0].country_name
+         };
+         insertPhysicianList({phyObject:phyObject})
+         .then(result => {
+             var res =result;
+             console.log(res);
+             if(res.status=='success'){
+                 this.succesMsg=res.message;
+                 this.errorMsg='';
+             }else{
+                 this.errorMsg='Physician Already Added';
+                 this.succesMsg='';
+             }
+ 
+         }).catch(error => {
+             this.error = error;
+             console.log(error);
+            
+         });
+         
+        }
+        
+     }
+}
--- force-app/main/default/lwc/searchPhysicianLWC/searchPhysicianLWC.js
+++ force-app/main/default/lwc/searchPhysicianLWC/searchPhysicianLWC.js
@@ -5,33 +5,50 @@ export default class SearchPhysicianLWC extends LightningElement {
     @track searchPhy='';
     @track spinner=false;
     @track physicianList=[];
+    @track showButton=false;
+    @track noInputMsg='';
+  
 
     handleLoad(physicianName) {
+       this.showButton=true;
         this.spinner=true;
         getPhysicianList({name:physicianName})
             .then(result => {
                 this.spinner=false;
                 this.physicianList = result;
+                this.showButton=false;
+                this.noInputMsg='';
                 console.log(result);
+                if(result==null){
+                    this.noInputMsg='There are no search results';
+                }else{
+                    this.noInputMsg='';
+                }
             })
             .catch(error => {
                 this.error = error;
                 console.log(error);
+               
             });
     }
     connectedCallback(){
       
     }
+    
+   
+    
     physicianChange(event){
         this.searchPhy = event.target.value;
+
      }
+    
     handleSearchPhysician(){
         if(this.searchPhy!=''&&this.searchPhy!=null){
+           this.noInputMsg='';
            this.handleLoad(this.searchPhy);
         }else{
-
+            this.noInputMsg='Please enter physician name';
         }
-        console.log('hi');
         
     }
      
